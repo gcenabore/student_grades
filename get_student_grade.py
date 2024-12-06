d7bc4ab95b304a4224d0b003e90ba07f8dc476ee
gcenabore <gianenabore30@gmail.com>
2024-12-07 02:35:24 +0800

    ask the user if they want another input

--- get_student_grade.py
+++ get_student_grade.py
@@ -2,51 +2,58 @@
 
 def get_student_grade():
 
-    try:
-        grade_input = float(input("Please enter your grade: "))
+    while True:
+        try:
+            grade_input = float(input("Please enter your grade: "))
 
-        if 50 <= grade_input <= 100:
+            if 50 <= grade_input <= 100:
 
-            if grade_input >= 97:
-                result = "PASSED - 1.00"
+                if grade_input >= 97:
+                    result = "PASSED - 1.00"
+                
+                elif 94 <= grade_input <97:
+                    result = "PASSED - 1.25"
+                
+                elif 91 <= grade_input < 94:
+                    result = "PASSED - 1.5"
+
+                elif 88 <= grade_input < 91:
+                    result = "PASSED - 1.75"
+
+                elif 85 <= grade_input < 88:
+                    result = "PASSED - 2.00"
+                
+                elif 82 <= grade_input < 85:
+                    result = "PASSED - 2.25"
+                
+                elif 79 <= grade_input < 82:
+                    result = "PASSED - 2.5"
+                
+                elif 76 <= grade_input < 79:
+                    result = "PASSED - 2.75"
+
+                elif 75 <= grade_input < 76:
+                    result = "PASSED - 3.00"
+
+                elif grade_input < 75:
+                    result = "FAILED - 5.00"
             
-            elif 94 <= grade_input <97:
-                result = "PASSED - 1.25"
-            
-            elif 91 <= grade_input < 94:
-                result = "PASSED - 1.5"
-
-            elif 88 <= grade_input < 91:
-                result = "PASSED - 1.75"
-
-            elif 85 <= grade_input < 88:
-                result = "PASSED - 2.00"
-            
-            elif 82 <= grade_input < 85:
-                result = "PASSED - 2.25"
-            
-            elif 79 <= grade_input < 82:
-                result = "PASSED - 2.5"
-            
-            elif 76 <= grade_input < 79:
-                result = "PASSED - 2.75"
-
-            elif 75 <= grade_input < 76:
-                result = "PASSED - 3.00"
-
-            elif grade_input < 75:
-                result = "FAILED - 5.00"
+                else:
+                    return None
+                
+                print(f"Your Grade is equivalent to {result}")
         
             else:
-                return None
-            
-            print(f"Your Grade is equivalent to {result}")
-    
-        else:
-            print("INVALID: Enter a grade from 50-100 only.")
-            
-    except ValueError:
-        print("ERROR: Grade must be Digit only")
+                print("INVALID: Enter a grade from 50-100 only.")
 
+        except ValueError:
+            print("ERROR: Grade must be Digit only")
+
+        another_input = input("Do you want to input another Grade? (Yes/No): ").strip().lower()
+
+        if another_input != 'yes':
+            print("THANK YOU FOR USING THIS CODE TO GET YOUR GPA")
+            break
+    
    
 get_student_grade()
