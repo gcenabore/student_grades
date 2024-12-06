920882b7dbb90f2a5ef248cc031539798495ff54
gcenabore <gianenabore30@gmail.com>
2024-12-07 01:42:55 +0800

    ask the user to input the grades, error if grades is not digit.check if the code is working

--- get_student_grade.py
+++ get_student_grade.py
@@ -1,1 +1,14 @@
 
+
+def get_student_grade():
+
+    try:
+        grade_input = float(input("Please enter your grade: "))
+
+        print(grade_input)
+        
+    except ValueError:
+        print("ERROR: Grade must be Digit only")
+
+   
+get_student_grade()
