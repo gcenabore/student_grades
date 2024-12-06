6c8ecab23cdd2705ba376e111b79202da0ebcf44
gcenabore <gianenabore30@gmail.com>
2024-12-07 01:46:08 +0800

    results for passed GPA 1.0

--- get_student_grade.py
+++ get_student_grade.py
@@ -5,8 +5,12 @@ def get_student_grade():
     try:
         grade_input = float(input("Please enter your grade: "))
 
-        print(grade_input)
+        if 50 <= grade_input <= 100:
+
+            if grade_input >= 79:
+                print("PASSED: GPA is 1.00")
         
+
     except ValueError:
         print("ERROR: Grade must be Digit only")
 
